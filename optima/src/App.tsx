6d01be913b2deaf8f18cdce09026e3cbd7c131dc
Ammar <ammar.alshareef2021@outlook.com>
2024-04-16 21:19:53 +0200

    optima 1st

---
+++ optima/src/App.tsx
@@ -0,0 +1,27 @@
+import { useState } from 'react'
+
+import './App.css'
+
+function App() {
+
+
+  return (
+    <>
+
+      <h1>This is Optima</h1>
+      <div className="card">
+        <h2> in this app user can do the following:</h2>
+        <ul>
+          <li>Assign cost and profit equations</li>
+          <li>Get optima points</li>
+          <li>Displays a full 3d or 2d resulting objective equation plotting</li>
+          <li>Displays live animation of particle swarms optimization action</li>
+        </ul>
+
+      </div>
+
+    </>
+  )
+}
+
+export default App
